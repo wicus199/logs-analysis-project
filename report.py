f5b56e9b56e9e319ad984a49f8ac0ea7cb78af7c
Wicus-Laptop <wicus92@gmail.com>
2017-06-25 17:32:41 +0200

    Add the query for the third question to the script
    Query isn't finalised yet. Still need to add better formatting to the dates
    column. At the moment, only a day is given with no month or year.

--- report.py
+++ report.py
@@ -13,7 +13,7 @@ c.execute("select articles.title, count(*) as artCount "
           "from articles, log where articles.slug = substring(log.path, 10) "
           "group by articles.title order by artCount desc limit 3")
 
-# Fetch the result of the first query
+# Fetch the result of the first query and store in articles
 articles = c.fetchall()
 
 c.execute("select authors.name, sum(articleSum.artViews) as authViews "
@@ -21,17 +21,32 @@ c.execute("select authors.name, sum(articleSum.artViews) as authViews "
           "where articleSum.title = articles.title and articles.author = authors.id "
           "group by authors.name order by authViews desc")
 
+# Fetch the result of the second query and store in authors
 authors = c.fetchall()
 
-# Close the database
+# Execute the query for the third question
+c.execute("select all_requests.date_part,(error_requests.num_errors*100)::numeric/all_requests.num_requests as percent_error "
+          "from error_requests, all_requests where error_requests.date_part = all_requests.date_part "
+          "and (error_requests.num_errors*100)::numeric/all_requests.num_requests > 1 "
+          "order by all_requests.date_part")
+
+# Fetch the result for the third query and store in errors
+errors = c.fetchall()
+
+# Close the news database
 db.close()
 
 print("1. What are the three most popular articles of all time?\n")
-# for statement to print the results of the query in a structured format
+# for statement to print the results of top three articles in a structured format
 for row in articles:
     print('\t"{}" - {} views'.format(row[0], row[1]))
 
 print("\n2. Who are the most popular article authors of all time?\n")
-# for statement to print the results of each author's views
+# for statement to print the results of each author's total views
 for row in authors:
     print('\t"{}" - {} views'.format(row[0], row[1]))
+
+print("\n3. On what days did more than 1% of requests lead to errors?\n")
+# for statement to print the days on which more than 1% of requests resulted in an error
+for row in errors:
+    print("\t{} July - {:.2f}% errors".format(int(row[0]),row[1]))
