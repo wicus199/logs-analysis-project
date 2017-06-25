0dd1afde1fa1f447b9da4280ab0f2364c4cfbece
Wicus-Laptop <wicus92@gmail.com>
2017-06-25 20:15:19 +0200

    Change code to adhere to PEP8 guidelines
    Nothing functional was changed in the code, only line lengths and whitespace.
    This was done to adhere to PEP8 guidelines, which is a requirement of the
    project. Some changes are unnecessary according to me, where I had to split a
    mathematical expression on 2 occasions in the code. However, I gave priority to
    the project requirements and split the expressions over 2 lines.

--- report.py
+++ report.py
@@ -3,7 +3,6 @@
 import psycopg2
 import time
 
-
 DBNAME = "news"
 
 # Connect to the database
@@ -20,16 +19,21 @@ articles = c.fetchall()
 
 c.execute("select authors.name, sum(articleSum.artViews) as authViews "
           "from articleSum, articles, authors "
-          "where articleSum.title = articles.title and articles.author = authors.id "
+          "where articleSum.title = articles.title "
+          "and articles.author = authors.id "
           "group by authors.name order by authViews desc")
 
 # Fetch the result of the second query and store in authors
 authors = c.fetchall()
 
 # Execute the query for the third question
-c.execute("select all_requests.time,(error_requests.num_errors*100)::numeric/all_requests.num_requests as percent_error "
-          "from error_requests, all_requests where error_requests.time = all_requests.time "
-          "and (error_requests.num_errors*100)::numeric/all_requests.num_requests > 1 "
+c.execute("select "
+          "all_requests.time,(error_requests.num_errors*100)::numeric"
+          "/all_requests.num_requests as percent_error "
+          "from error_requests, all_requests "
+          "where error_requests.time = all_requests.time "
+          "and (error_requests.num_errors*100)::numeric"
+          "/all_requests.num_requests > 1 "
           "order by all_requests.time")
 
 # Fetch the result for the third query and store in errors
@@ -39,7 +43,7 @@ errors = c.fetchall()
 db.close()
 
 print("1. What are the three most popular articles of all time?\n")
-# for statement to print the results of top three articles in a structured format
+# for statement to print results of top three articles in a structured format
 for row in articles:
     print('\t"{}" - {} views'.format(row[0], row[1]))
 
@@ -49,6 +53,10 @@ for row in authors:
     print('\t"{}" - {} views'.format(row[0], row[1]))
 
 print("\n3. On which days did more than 1% of requests lead to errors?\n")
-# for statement to print the days on which more than 1% of requests resulted in an error
+# for statement to print the days on which
+# more than 1% of requests resulted in an error
 for row in errors:
-    print("\t{} {} {} - {:.2f}% errors".format(row[0].strftime("%d"),row[0].strftime("%B"), row[0].strftime("%Y"), row[1]))
+    print("\t{} {} {} - {:.2f}% errors".format(row[0].strftime("%d"),
+                                               row[0].strftime("%B"),
+                                               row[0].strftime("%Y"),
+                                               row[1]))
