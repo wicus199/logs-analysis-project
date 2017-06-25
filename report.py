b3d42c2ed3610ccea0db411564032b8ba270041d
Wicus-Laptop <wicus92@gmail.com>
2017-06-25 15:25:09 +0200

    Add SQL query for the second question
    This query is used to answer the question on who is author with the most article
    views. The query uses a view created in the database. The insturctions on how to
    create the view is described in the README.

--- report.py
+++ report.py
@@ -13,12 +13,25 @@ c.execute("select articles.title, count(*) as artCount "
           "from articles, log where articles.slug = substring(log.path, 10) "
           "group by articles.title order by artCount desc limit 3")
 
-# Fetch the result of the query
+# Fetch the result of the first query
 articles = c.fetchall()
+
+c.execute("select authors.name, sum(articleSum.artViews) as authViews "
+          "from articleSum, articles, authors "
+          "where articleSum.title = articles.title and articles.author = authors.id "
+          "group by authors.name order by authViews desc")
+
+authors = c.fetchall()
+
 # Close the database
 db.close()
 
 print("1. What are the three most popular articles of all time?\n")
-# For statement to print the results of the query in a structured format
+# for statement to print the results of the query in a structured format
 for row in articles:
+    print('\t"{}" - {} views'.format(row[0], row[1]))
+
+print("\n2. Who are the most popular article authors of all time?\n")
+# for statement to print the results of each author's views
+for row in authors:
     print('\t"{}" - {} views'.format(row[0], row[1]))
